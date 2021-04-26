455630974594d60041b87d3b0156468546ce49ec
BELLO8 <54768908+BELLO8@users.noreply.github.com>
2021-04-26 11:11:06 +0000

    Delete Parking directory

--- Parking/db.h
+++
@@ -1,6 +0,0 @@
-#ifndef DB_H
-#define DB_H
-
-void db(MYSQL *db);
-
-#endif
--- Parking/parking.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARKING_H
-#define PARKING_H
-
-void entrer_parking(MYSQL *c);
-void place_disp(MYSQL *connexion);
-void sortir_parking();
-void nbre_place(MYSQL *c , char *type);
-
-#endif
